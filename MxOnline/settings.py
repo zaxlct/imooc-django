bd718fe6556b1d37ea2d67a584ea0543f041017c
zaxlct <zaxlct@foxmail.com>
2017-01-07 19:52:00 +0800

    完成表设计

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'courses',
+    'organization',
+    'operation',
 ]
 AUTH_USER_MODEL = 'users.UserProfile'
 
---
+++ apps/operation/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OperationConfig(AppConfig):
+    name = 'operation'
