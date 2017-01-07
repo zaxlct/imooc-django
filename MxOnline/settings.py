f4943ffc3abea109a44cd1677aca6b7968f68761
zaxlct <zaxlct@foxmail.com>
2017-01-07 17:51:29 +0800

    课程APP 表结构

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'courses',
 ]
 AUTH_USER_MODEL = 'users.UserProfile'
 
---
+++ courses/models.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from  datetime import datetime
+
+from django.db import models
+
+# Create your models here.
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=50, verbose_name=u'课程名')
+    desc = models.CharField(max_length=300, verbose_name=u'课程描述')
+    # TextField 不限制输入长度
+    detail = models.TextField(verbose_name=u'课程详情')
+    degree = models.CharField(choices=(('cj', u'初级'), ('zj', u'中级'), ('gj', u'高级')), max_length=2)
+    learn_times = models.IntegerField(default=0, verbose_name=u'学习时长(分钟数)')
+    students = models.IntegerField(default=0, verbose_name=u'学习人数')
+    fav_nums = models.IntegerField(default=0, verbose_name=u'收藏人数')
+    image = models.ImageField(upload_to='courses/%Y/%m', verbose_name=u'封面图', max_length=100)
+    click_nums = models.IntegerField(default=0, verbose_name=u'点击数')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
+
+    class Meta:
+        verbose_name = u'课程'
+        verbose_name_plural = verbose_name
+
+
+# 章节信息
+# 课程和章节是一对多的关系
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, verbose_name=u'课程')
+    name = models.CharField(max_length=100, verbose_name=u'章节名')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
+
+    class Meta:
+        verbose_name = u'章节'
+        verbose_name_plural = verbose_name
+
+
+class Video(models.Model):
+    course = models.ForeignKey(Lesson, verbose_name=u'章节')
+    name = models.CharField(max_length=100, verbose_name=u'视频名')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
+
+    class Meta:
+        verbose_name = u'视频'
+        verbose_name_plural = verbose_name
+
+
+class CourseResource(models.Model):
+    course = models.ForeignKey(Course, verbose_name=u'课程')
+    name = models.CharField(max_length=100, verbose_name=u'课件名')
+    download = models.FileField(upload_to='course/resource/%Y/%m', verbose_name=u'资源文件', max_length=100)
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
+
+    class Meta:
+        verbose_name = u'课程资源'
+        verbose_name_plural = verbose_name
