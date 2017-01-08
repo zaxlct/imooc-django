20bc4af58cb3fdae027f484aa445f07876b3b929
zaxlct <zaxlct@foxmail.com>
2017-01-08 18:23:41 +0800

    完成 operatoon adminx 配置

--- apps/operation/models.py
+++ apps/operation/models.py
@@ -58,7 +58,6 @@ class UserMessage(models.Model):
     has_read = models.BooleanField(default=False, verbose_name=u'是否已读')
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
 
-
     class Meta:
         verbose_name = u'用户消息'
         verbose_name_plural = verbose_name
