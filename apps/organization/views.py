3fb4d9a9420449c507a62aa073ebe89f11887437
zaxlct <zaxlct@foxmail.com>
2017-01-11 15:50:15 +0800

    7-5 分页功能

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -2,6 +2,7 @@
 
 from django.shortcuts import render
 from django.views.generic import View
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import CourseOrg, CityDict
 
@@ -13,15 +14,25 @@ class OrgView(View):
     def get(self, request):
         # 课程机构
         all_orgs = CourseOrg.objects.all()
+
         org_nums = all_orgs.count()
         # 这个 count 方法是 django 提供的
         # 实际上执行了一条 SQL 语句 SELECT count(*) from table_name
         # 和 python 提供的 count 函数有区别
 
+        # 对课程机构进行分页
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+
+        p = Paginator(all_orgs, 2, request=request)
+        orgs = p.page(page)
+
         # 城市
         all_citys = CityDict.objects.all()
         return render(request, 'org-list.html', {
-            'all_orgs': all_orgs,
+            'all_orgs': orgs,
             'all_citys': all_citys,
             'org_nums': org_nums,
         })
