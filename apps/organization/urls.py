f822cf39ff65f8b9c46db290c1c978e3bca6ac56
zaxlct <zaxlct@foxmail.com>
2017-01-12 17:43:04 +0800

    7-12课程收藏功能

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -3,7 +3,7 @@ __author__ = 'zaxlct'
 __date__ = '2017/1/11 下午8:46'
 
 from django.conf.urls import url, include
-from .views import OrgView, AddUserAskView, OrgHomeView, OrgCourseView, OrgDescView, OrgTeacherView
+from .views import OrgView, AddUserAskView, OrgHomeView, OrgCourseView, OrgDescView, OrgTeacherView, AddFavView
 
 
 urlpatterns = [
@@ -14,4 +14,7 @@ urlpatterns = [
     url(r'^course/(?P<org_id>\d+)/$', OrgCourseView.as_view(), name='org_course'),
     url(r'^desc/(?P<org_id>\d+)/$', OrgDescView.as_view(), name='org_desc'),
     url(r'^teacher/(?P<org_id>\d+)/$', OrgTeacherView.as_view(), name='org_teacher'),
+
+    # 课程机构收藏/取消收藏
+    url(r'^add_fav/$', AddFavView.as_view(), name='add_fav'),
 ]
