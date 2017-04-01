488d8584b874522a5bbaa8e07da46353517689c6
zaxlct <zaxlct@foxmail.com>
2017-04-01 15:16:42 +0800

    创建 apps

--- imooc/settings.py
+++ imooc/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'courses',
+    'operation',
+    'organization',
+    'users'
 ]
 
 MIDDLEWARE = [
