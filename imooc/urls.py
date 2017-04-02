1ab6d2263878431dfcafa809a80a67e7d98ff813
zaxlct <zaxlct@foxmail.com>
2017-04-02 20:15:57 +0800

    配置首页

--- imooc/urls.py
+++ imooc/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 # from django.contrib import admin
+from django.views.generic import TemplateView
 
 import xadmin
 
 urlpatterns = [
     url(r'^admin/', xadmin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index')
 ]
