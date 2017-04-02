f1e869d75378bad33832898a11ec2044d6058db2
zaxlct <zaxlct@foxmail.com>
2017-04-02 18:29:42 +0800

    xadmin 后台页面配置

---
+++ apps/courses/adminx.py
@@ -0,0 +1,38 @@
+__author__ = 'zaxlct'
+__date__ = '2017/4/2 下午5:08'
+
+import xadmin
+from .models import Course, Lesson, Video, CourseResource
+
+
+class CourseAdmin():
+    list_display = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students', 'fav_nums',
+                    'click_nums', 'add_time']
+    search_fields = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students', 'fav_nums',
+                    'click_nums']
+    list_filter = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students', 'fav_nums', 'click_nums', 'add_time']
+
+
+class LessonAdmin():
+    list_display = ['course', 'name', 'add_time']
+    search_fields = ['course', 'name']
+    # course 是一个对象，xadmin 不能搜索，需要指定搜索 course 对象里哪一个属性
+    list_filter = ['course__name', 'name', 'add_time']
+
+
+class VideoAdmin():
+    list_display = ['lesson', 'name', 'add_time']
+    search_fields = ['lesson', 'name']
+    list_filter = ['lesson', 'name', 'add_time']
+
+
+class CourseResourceAdmin():
+    list_display = ['course', 'name', 'download', 'add_time']
+    search_fields = ['course', 'name', 'download']
+    list_filter = ['course__name', 'name', 'add_time']
+
+
+xadmin.site.register(Course, CourseAdmin)
+xadmin.site.register(Lesson, LessonAdmin)
+xadmin.site.register(Video, VideoAdmin)
+xadmin.site.register(CourseResource, CourseResourceAdmin)
---
+++ apps/users/adminx.py
@@ -0,0 +1,33 @@
+import xadmin
+from xadmin import views
+
+from .models import EmailVerifyRecord, Banner
+
+
+### adminx 全局配置
+class BaseSetting():
+    enable_themes = True
+    use_bootswatch = True
+
+
+class GlobalSettings():
+    site_title = '慕学后台管理系统'
+    site_footer = '慕学在线网'
+    menu_style = 'accordion'
+###
+
+class EmailVerifyRecordAdmin():
+    list_display = ['code', 'email', 'send_type', 'send_time']
+    search_fields = ['code', 'email', 'send_type']
+    list_filter = ['code', 'email', 'send_type', 'send_time']
+
+
+class BannerAdmin():
+    list_display = ['title', 'image', 'url', 'index', 'add_time']
+    search_fields = ['title', 'image', 'url', 'index']
+    list_filter = ['title', 'image', 'url', 'index', 'add_time']
+
+xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
+xadmin.site.register(Banner, BannerAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSettings)
