819b6d486311ef5b32b69e65dc0d35f496d0a5d0
zaxlct <zaxlct@foxmail.com>
2017-04-02 20:30:49 +0800

    配置登录、首页跳转 URL

--- imooc/urls.py
+++ imooc/urls.py
@@ -21,5 +21,6 @@ import xadmin
 
 urlpatterns = [
     url(r'^admin/', xadmin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index')
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^login/$', TemplateView.as_view(template_name='login.html'), name='login')
 ]
