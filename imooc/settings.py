9f6bd76a063bd3b8c9bdfbf31193eb4fd642282b
zaxlct <zaxlct@foxmail.com>
2017-04-02 15:36:26 +0800

    更改语言和时区

--- imooc/settings.py
+++ imooc/settings.py
@@ -116,15 +116,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False # 数据库取本地时间
 
 
 # Static files (CSS, JavaScript, Images)
