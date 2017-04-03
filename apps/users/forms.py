3ccd0ef5d8dc1cdbab0d42af5eb44599a631f4fd
zaxlct <zaxlct@foxmail.com>
2017-04-03 16:45:58 +0800

    注册表单验证

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -2,8 +2,15 @@ __author__ = 'zaxlct'
 __date__ = '2017/4/3 下午12:29'
 
 from django import forms
+from captcha.fields import CaptchaField
 
 
 class LoginForm(forms.Form):
     username = forms.CharField(required=True)
     password = forms.CharField(required=True, min_length=5)
+
+
+class RegisterForm(forms.Form):
+    email = forms.EmailField(required=True)
+    password = forms.CharField(required=True, min_length=5)
+    captcha = CaptchaField(error_messages={'invalid': '验证码错误！'})
