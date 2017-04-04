c636653c8e132f8336814e97b5d4a464a6d93c6d
zaxlct <zaxlct@foxmail.com>
2017-04-04 11:21:23 +0800

    找回密码邮件逻辑

--- apps/utils/email_send.py
+++ apps/utils/email_send.py
@@ -26,6 +26,15 @@ def send_register_email(email, send_type='register'):
             # TODO
             print('发送成功')
 
+    elif send_type == 'forget':
+        email_title = '慕雪在线网密码重置链接'
+        email_body = '请点击下面的链接重置你的密码：http://127.0.0.1:8000/reset/{0}'.format(code)
+
+        send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
+        if send_status:
+            # TODO 提示发送成功
+            pass
+
 
 def random_str(random_length=16):
     code = ''
