c3e4bcfe417f546baab1c7a7087234eae9cb4913
zaxlct <zaxlct@foxmail.com>
2017-04-05 16:24:39 +0800

    筛选课程机构

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -10,9 +10,30 @@ from .models import CourseOrg, CityDict
 class OrgView(View):
     def get(self, request):
         all_orgs = CourseOrg.objects.all()
+        hot_orgs = all_orgs.order_by('-click_nums')[:3]
         all_citys = CityDict.objects.all()
+
+        # 取出筛选城市
+        city_id = request.GET.get('city', '')
+        if city_id:
+            all_orgs = all_orgs.filter(city_id=int(city_id))
+
+        # 课程机构类别筛选
+        category = request.GET.get('ct', '')
+        if category:
+            all_orgs = all_orgs.filter(category=category)
+
+        # 排序
+        sort = request.GET.get('sort', '')
+        if sort == 'students':
+            all_orgs = all_orgs.order_by('-students')
+        elif sort == 'courses':
+            all_orgs = all_orgs.order_by('-course_nums')
+
+        #筛选完成之后再进行统计
         org_nums = all_orgs.count()
 
+        # 分页
         try:
             page = request.GET.get('page', 1)
         except PageNotAnInteger:
@@ -26,4 +47,8 @@ class OrgView(View):
             'all_orgs': orgs,
             'all_citys': all_citys,
             'org_nums': org_nums,
+            'city_id': city_id,
+            'sort': sort,
+            'category': category,
+            'hot_orgs': hot_orgs,
         })
