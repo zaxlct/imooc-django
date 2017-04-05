6f06a5e1fac4ed4a57e1e2dca2956c9101fb8f90
zaxlct <zaxlct@foxmail.com>
2017-04-05 14:42:50 +0800

    数据表表更

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 
 class Course(models.Model):
-    name = models.CharField(max_length=50, verbose_name='课程名')
+    name = models.CharField(max_length=52, verbose_name='课程名字')
     desc =  models.CharField(max_length=300, verbose_name='课程描述')
     detail = models.TextField(verbose_name='课程详情')
     degree = models.CharField(choices=(('cj', '初级'), ('zj', '中级'), ('gj', '高级')), max_length=2, verbose_name='难度')
--- apps/organization/models.py
+++ apps/organization/models.py
@@ -20,6 +20,7 @@ class CityDict(models.Model):
 
 class CourseOrg(models.Model):
     name = models.CharField(max_length=50, verbose_name='机构名称')
+    category = models.CharField(max_length=20, choices=( ('pxjg', '培训机构'), ('gr', '个人'), ('gx', '高校') ), default='pxjg', verbose_name='机构类别' )
     desc = models.TextField(verbose_name='机构描述')
     click_nums = models.IntegerField(default=0, verbose_name='点击数')
     fav_nums = models.IntegerField(default=0, verbose_name='收藏数')
@@ -27,6 +28,9 @@ class CourseOrg(models.Model):
     address = models.CharField(max_length=150, verbose_name='机构地址')
     city = models.ForeignKey(CityDict, verbose_name='所在城市')
     add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+    students = models.IntegerField(default=0, verbose_name='学习人数')
+    # 课程机构的课程数应该是计算出来的，不应该是写死的
+    course_nums = models.IntegerField(default=0, verbose_name='课程数')
 
     class Meta:
         verbose_name = '课程机构'
@@ -45,6 +49,7 @@ class Teacher(models.Model):
     points = models.CharField(max_length=50, verbose_name='教学特点')
     click_nums = models.IntegerField(default=0, verbose_name='点击数')
     fav_nums = models.IntegerField(default=0, verbose_name='收藏数')
+    image = models.ImageField(default='', upload_to='teacher/%Y/%m', verbose_name='头像', max_length=100)
     add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
 
     class Meta:
