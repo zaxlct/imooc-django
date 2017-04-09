d459b92da38d06d312a5d8f0acb3f765fa1f83d1
zaxlct <zaxlct@foxmail.com>
2017-04-09 14:34:46 +0800

    更改字段名

--- static/js/deco-user.js
+++ static/js/deco-user.js
@@ -174,8 +174,8 @@ $(function () {
       success: function (data) {
         if (data.nick_name) {
           _showValidateError($('#nick_name'), data.nick_name);
-        } else if (data.birday) {
-          _showValidateError($('#birth_day'), data.birday);
+        } else if (data.birthday) {
+          _showValidateError($('#birth_day'), data.birthday);
         } else if (data.address) {
           _showValidateError($('#address'), data.address);
         } else if (data.status == "failure") {
