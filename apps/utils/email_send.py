5f38fffc7bd39a2e392d2829e717871f15ab6645
zaxlct <zaxlct@foxmail.com>
2017-05-10 16:21:00 +0800

    发送邮件注意点

--- apps/utils/email_send.py
+++ apps/utils/email_send.py
@@ -20,6 +20,7 @@ def send_register_email(email, send_type='register'):
 
     if send_type == 'register':
         email_title = '慕雪在线网注册激活链接'
+        # http://118.89.105.65 是我自己的服务器 IP 地址，你部署的时候，请换成你自己的 IP 或 域名
         email_body = '请点击下面的链接（如果是线上环境，请把 http://127.0.0.1:8000 替换成 http://118.89.105.65 ）激活你的账号：http://127.0.0.1:8000/active/{0}'.format(code)
         send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
         if send_status:
