e79d0816cabb6afdbd50dc630a905d7f0d2a5caa
zaxlct <zaxlct@foxmail.com>
2017-05-10 16:19:48 +0800

    更改发送邮件提示

--- apps/utils/email_send.py
+++ apps/utils/email_send.py
@@ -20,7 +20,7 @@ def send_register_email(email, send_type='register'):
 
     if send_type == 'register':
         email_title = '慕雪在线网注册激活链接'
-        email_body = '请点击下面的链接激活你的账号：http://127.0.0.1:8000/active/{0}'.format(code)
+        email_body = '请点击下面的链接（如果是线上环境，请把 http://127.0.0.1:8000 替换成 http://118.89.105.65 ）激活你的账号：http://127.0.0.1:8000/active/{0}'.format(code)
         send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
         if send_status:
             # TODO
@@ -28,7 +28,7 @@ def send_register_email(email, send_type='register'):
 
     elif send_type == 'forget':
         email_title = '慕雪在线网密码重置链接'
-        email_body = '请点击下面的链接重置你的密码：http://127.0.0.1:8000/reset/{0}'.format(code)
+        email_body = '请点击下面的链接（如果是线上环境，请把 http://127.0.0.1:8000 替换成 http://118.89.105.65 ）重置你的密码：http://127.0.0.1:8000/reset/{0}'.format(code)
 
         send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
         if send_status:
