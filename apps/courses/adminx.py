3a36a907655195db96d907f51dab7fc62ebc9511
zaxlct <zaxlct@foxmail.com>
2017-05-10 15:49:42 +0800

    后台添加课程的时候可以顺便添加章节
    添加课程的时候可以顺便添加课程资源

--- apps/courses/adminx.py
+++ apps/courses/adminx.py
@@ -4,29 +4,54 @@ __date__ = '2017/4/2 下午5:08'
 import xadmin
 from .models import Course, Lesson, Video, CourseResource
 
+# 添加课程的时候可以顺便添加章节
+class LessonInline:
+    model = Lesson
+    extra = 0
 
-class CourseAdmin():
+
+# 添加课程的时候可以顺便添加课程资源
+class CourseResourceInline:
+    model = CourseResource
+    extra = 0
+
+
+class CourseAdmin:
     list_display = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students', 'fav_nums',
                     'click_nums', 'add_time']
     search_fields = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students', 'fav_nums',
                     'click_nums']
     list_filter = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students', 'fav_nums', 'click_nums', 'add_time']
 
+    ordering = ['-click_nums']
 
-class LessonAdmin():
+    # readonly_fields 和 exclude 的字段不要重复，否则会冲突
+    readonly_fields = ['click_nums']
+    exclude = ['fav_nums']
+
+    #Inline # 添加课程的时候可以顺便添加章节、课程资源
+    inlines = [LessonInline, CourseResourceInline]
+
+    def queryset(self):
+        qs = super(CourseAdmin, self).queryset()
+        qs = qs.filter(is_banner=False)
+        return qs
+
+
+class LessonAdmin:
     list_display = ['course', 'name', 'add_time']
     search_fields = ['course', 'name']
     # course 是一个对象，xadmin 不能搜索，需要指定搜索 course 对象里哪一个属性
     list_filter = ['course__name', 'name', 'add_time']
 
 
-class VideoAdmin():
+class VideoAdmin:
     list_display = ['lesson', 'name', 'add_time']
     search_fields = ['lesson', 'name']
     list_filter = ['lesson', 'name', 'add_time']
 
 
-class CourseResourceAdmin():
+class CourseResourceAdmin:
     list_display = ['course', 'name', 'download', 'add_time']
     search_fields = ['course', 'name', 'download']
     list_filter = ['course__name', 'name', 'add_time']
